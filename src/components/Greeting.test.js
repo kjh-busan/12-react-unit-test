2fdbd44e4f66b363c3a7ab60797feacd5120eacd
kjh-busan <picsmash7@gmail.com>
2023-04-21 17:32:44 +0900

    feat: add test case passed

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -12,9 +12,21 @@ describe("Greeting component", () => {
     // Assert
     const helloWorldElement = screen.getByText("Hello World!");
     expect(helloWorldElement).toBeInTheDocument();
-
-    test();
-
-    test();
   });
+
+  test("renders good to see you if the button was NOT clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    // ...Nothings...
+
+    // Assert
+    const greetingNotRenderingElement = screen.getByText("good to see you", {
+      exact: false,
+    });
+    expect(greetingNotRenderingElement).toBeInTheDocument();
+  });
+
+  // test();
 });
