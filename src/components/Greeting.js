baf9b35a4d51d9bcf70544654583a15a36c9e7cd
kjh-busan <picsmash7@gmail.com>
2023-04-21 17:16:09 +0900

    feat: add test case1

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
+
 const Greeting = () => {
+  const [changedText, setChangedText] = useState(false);
+  const changedTextHandler = () => {};
+
   return (
     <div>
       <h2>Hello World!</h2>
       <p>It's good to see you</p>
+      <button onClick={changedTextHandler}>Change Text!</button>
     </div>
   );
 };
