05b4609e72ffe374c828def4ca76bb0508f9ddc4
kjh-busan <picsmash7@gmail.com>
2023-04-22 14:31:30 +0900

    feat: toBeNull case test

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -31,7 +31,22 @@ describe("Greeting component", () => {
     userEvent.click(buttonElement);
 
     // Assert
-    const outputElement = screen.getByText("Changed!", { exact: false });
+    const outputElement = screen.getByText("Changed!");
     expect(outputElement).toBeInTheDocument();
+  });
+
+  test("does not render 'good to see you!' if the button was clicked", () => {
+    //Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
