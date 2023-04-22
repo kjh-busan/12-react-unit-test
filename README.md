22771d04b81686018ac21211004a078ae2c87c05
kjh-busan <picsmash7@gmail.com>
2023-04-22 14:52:19 +0900

    feat: async jest test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 12-react-unit-test
+
+# 참고자료(영어)
+https://www.w3.org/TR/html-aria/#docconformance
--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -1,3 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import Async from "./Async";
+
 describe("Async component", () => {
-  test("renders posts if request succeeds", () => {});
+  test("renders posts if request succeeds", async () => {
+    render(<Async />);
+
+    const listItemElements = await screen.findAllByRole("list");
+    expect(listItemElements).not.toHaveLength(0);
+  });
 });
