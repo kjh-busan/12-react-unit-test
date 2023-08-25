50ae581e96765ba05e344aae51ecde2d501f269a
kjh-busan <picsmash7@gmail.com>
2023-08-25 21:02:36 +0900

    git practice

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Output from "./Output";
 
 const Greeting = () => {
-  const [changedText, setChangedText] = useState(false);
+  // const [changedText, setChangedText] = useState(false);
 
   const changedTextHandler = () => {
     // setChangedText(true);
