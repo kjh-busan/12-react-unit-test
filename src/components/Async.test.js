f923996927a0d26c74a052220492a429a3532329
kjh-busan <picsmash7@gmail.com>
2023-08-26 20:38:49 +0900

    git practice2

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -4,9 +4,9 @@ import Async from "./Async";
 describe("Async component", () => {
   test("renders posts if request succeeds", async () => {
     window.fetch = jest.fn();
-    window.fetch.mockResolvedValueOnce({
-      json: async () => [{ id: "p1", title: "Frist post" }],
-    });
+    // window.fetch.mockResolvedValueOnce({
+    //   json: async () => [{ id: "p1", title: "Frist post" }],
+    // });
     render(<Async />);
 
     const listItemElements = await screen.findAllByRole("list");
